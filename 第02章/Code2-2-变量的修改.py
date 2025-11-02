3575bba330ec23f1c69bfefc741c3e31c3160112
Ricardo <RicardoLu955@gmail.com>
2025-11-03 00:46:07 +0800

    第二章

---
+++ 第02章/Code2-2-变量的修改.py
@@ -0,0 +1,17 @@
+year = 2023
+print(year)
+
+year = 2024
+print(year)
+
+money = '十元'
+money = 10
+print(money)  # Python是弱类型语言
+# 不同类型的变量也可以进行修改、重新赋值，与类型无关。
+
+# 任务：满减
+price = 10.5
+weight = 7.5
+money = price * weight
+money = money - 5
+print("共消费： %.2f 元" % money)
---
+++ 第02章/Code2-5-整数.py
@@ -0,0 +1,17 @@
+# 整数（Integers）
+# 正数
+num1 = 10
+print(num1)
+
+# 负数
+num2 = -10
+print(num2)
+
+# 查看变量的数据类型
+print(type(num2))
+
+# 输入数字进行计算
+num3 = input("请输入一个数字：")
+print(type(num3))
+num3 = int(num3) + 10
+print(num3)
